e2d52cc61b108971589fb05f38d5fbc43371dc93
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-08 17:34:34 -0300

    Add *

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ const App = () => {
   console.log(import.meta.env.MODE);
   console.log(import.meta.env.VITE_API_HOST);
 
+  console.log("*");
   console.log(process.env.VITE_HOLA);
+  console.log("*");
 
   return <h1>Hello world</h1>;
 };
