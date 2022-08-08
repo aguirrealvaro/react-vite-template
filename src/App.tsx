dfac328bec1f07e46b29b4bdd1725a5f5f3c0a96
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-08 16:43:47 -0300

    Set envs to process.env

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 const App = () => {
-  console.log(import.meta.env.MODE);
-  console.log(import.meta.env.API_HOST);
+  //console.log(import.meta.env.MODE);
+  //console.log(import.meta.env.API_HOST);
+
+  console.log(process.env.MODE);
+  console.log(process.env.API_HOST);
+
 
   return <h1>Hello world</h1>;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
+const validEnvs = ["development", "production"];
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  if (!validEnvs.includes[mode]) {
+    const message = `Unsupported mode: "${mode}"`;
+    throw Error(message);
+  }
+
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    define: {
+      "process.env": env,
+    },
+  };
 });
