a77e9cf0f8c83314eead6366412e7939c539ce32
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-09 13:59:59 -0300

    Sort imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react-hooks"],
+  plugins: ["@typescript-eslint", "react-hooks", "import"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
@@ -17,8 +17,33 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "warn",
     "react-hooks/rules-of-hooks": "warn",
     "react-hooks/exhaustive-deps": "error",
-    "no-duplicate-imports": "error",
     "prettier/prettier": "error",
+    "import/no-duplicates": "error",
+    "import/no-unresolved": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [["external", "builtin"], "parent", ["index", "internal", "sibling"]],
+        pathGroups: [
+          {
+            pattern: "react-dom",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        "newlines-between": "never",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
   env: {
     node: true,
