70f7fccd1f839d051a45da054b73a43e7dc6fd47
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-08 22:51:38 -0300

    format

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ console.log(process.env.MODE);
 console.log(process.env.API_HOST);
 console.log(process.env.APP_VERSION);
 
-
 const App: FunctionComponent = () => <h1>Hello world</h1>;
 
 export default App;
