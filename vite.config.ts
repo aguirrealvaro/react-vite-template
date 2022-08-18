653300fd7a988ed8ee96069452b19c44aef213c9
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-18 12:35:00 -0300

    Run linter in all files

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const PACKAGE = require("./package.json");
 
