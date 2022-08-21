98bb3fe29ead60826f603f6b1944cba45dc20e6c
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-20 22:47:02 -0300

    Check types on pre commit

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,3 +1,6 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": ["npm run lint", "tsc-files --noEmit"],
+  "*.{js,jsx,ts,tsx}": [
+    "npm run lint",
+    "eslint --fix --plugin tsc --rule 'tsc/config: [2, {configFile: ./tsconfig.json}]'",
+  ],
 };
