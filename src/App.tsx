bfbb75ba31b2e4873f9909b3ebc1c88dbf5c9377
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-26 18:27:28 -0300

    Console log example

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ console.log("This is a preview url branch");
 console.log(process.env.MODE);
 console.log(process.env.API_HOST);
 console.log(process.env.APP_VERSION);
+console.log(process.env.EXAMPLE);
 
 const App: FunctionComponent = () => {
   return <h1>Hello world</h1>;
