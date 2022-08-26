4b2fe84136d60073f8d66f0c2857346d9399e841
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-26 18:38:24 -0300

    Revert changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable no-console */
 import { FunctionComponent } from "react";
 
-console.log("This is a preview url branch");
 console.log(process.env.MODE);
 console.log(process.env.API_HOST);
 console.log(process.env.APP_VERSION);
-console.log(process.env.EXAMPLE);
 
 const App: FunctionComponent = () => {
   return <h1>Hello world</h1>;
