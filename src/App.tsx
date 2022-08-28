9bff84243cc16658b17e1f067f86df372e0e31b3
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-28 18:24:46 -0300

    asdasd

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { FunctionComponent } from "react";
 console.log(process.env.MODE);
 console.log(process.env.API_HOST);
 console.log(process.env.APP_VERSION);
-console.log("preview");
+console.log("process.env.APP_VERSION");
 
 const App: FunctionComponent = () => {
   return <h1>Hello world</h1>;
