9c57d6e6fc4ff867e7a2fc62c75256c9385a0b35
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-27 19:55:00 -0300

    Install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-console */
 import { FunctionComponent } from "react";
+import "@/styles/globals.css";
 
 console.log(process.env.MODE);
 console.log(process.env.API_HOST);
 console.log(process.env.APP_VERSION);
 
 const App: FunctionComponent = () => {
-  return <h1>Hello world</h1>;
+  return <h1 className="text-xl">Hello world</h1>;
 };
 
 export default App;
